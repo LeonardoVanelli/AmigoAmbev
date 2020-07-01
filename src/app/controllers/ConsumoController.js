742ef4a13a8f020bedeb179664453ced34b9c1e5
Leonardo Vanelli <leonardo.vanelli123@gmail.com>
2020-07-01 19:51:29 -0300

    Consumo em bar

--- src/app/controllers/ConsumoController.js
+++ src/app/controllers/ConsumoController.js
@@ -3,6 +3,7 @@ import { Op } from 'sequelize';
 import Codigos from '../models/Codigo';
 import Usuario from '../models/User';
 import Atividade from '../models/Atividade';
+import Bebida from '../models/Bebida';
 
 class ConsumoController {
   async store(req, res) {
@@ -17,23 +18,44 @@ class ConsumoController {
     });
     if (!codigo) return res.status(500).send('Código inválido');
 
-    const usuario = await Usuario.findByPk(usuario_id);
+    const usuario = await Usuario.findByPk(usuario_id, {
+      attributes: ['id', 'name', 'pontos'],
+    });
     if (!usuario) return res.status(500).send('Usuário não encontrado');
 
     if (!noBar) {
-      codigo.update({
+      await codigo.update({
         usuario_utilizacao_id: usuario_id,
         data_hora_utilizado: new Date(),
       });
 
-      await Atividade.update({
+      await Atividade.create({
         tipo_processo_id: 2,
         usuario_id,
         validado: true,
       });
+
+      const bebida = await Bebida.findByPk(codigo.bebida_id, {
+        attributes: ['pontos_comercio'],
+      });
+
+      usuario.update({
+        pontos: usuario.pontos + bebida.pontos_comercio,
+      });
+    } else {
+      await codigo.update({
+        usuario_utilizacao_id: usuario_id,
+        data_hora_utilizado: new Date(),
+      });
+
+      await Atividade.create({
+        tipo_processo_id: 2,
+        usuario_id,
+        validado: false,
+      });
     }
 
-    res.send(req.body);
+    return res.send({ usuario });
   }
 }
 
