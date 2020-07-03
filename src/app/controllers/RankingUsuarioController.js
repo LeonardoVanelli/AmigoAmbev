8e6a16491365b6d2a503e7108ab7d89e090b9318
Leonardo Vanelli <leonardo.vanelli123@gmail.com>
2020-07-02 21:53:29 -0300

    Mostrar prosicao

--- src/app/controllers/RankingUsuarioController.js
+++ src/app/controllers/RankingUsuarioController.js
@@ -2,22 +2,43 @@ import User from '../models/User';
 
 class RankingUsuarioController {
   async show(req, res) {
-    const users = await User.findAll({
-      order: [['pontos', 'desc']],
-      limit: 10,
-      attributes: ['id', 'name', 'pontos'],
-    });
-
-    const finallUser = users.find(user => user.id === req.userId);
-
-    if (!finallUser) {
-      const user = await User.findByPk(req.userId, {
+    try {
+      const users = await User.findAll({
+        order: [['pontos', 'desc']],
+        limit: 10,
         attributes: ['id', 'name', 'pontos'],
       });
-      return res.send({ user, rank: users, finallUser });
-    }
 
-    return res.send({ rank: users });
+      const finallUser = users.find(user => {
+        return user.id === req.userId;
+      });
+
+      if (!finallUser) {
+        const user = await User.findByPk(req.userId, {
+          attributes: ['id', 'name', 'pontos'],
+        });
+
+        const todosUsuarios = await User.findAll({
+          order: [['pontos', 'desc']],
+        });
+        let posicao = 0;
+        todosUsuarios.find(userUtilizado => {
+          posicao++;
+          return userUtilizado.id === req.userId;
+        });
+
+        return res.send({
+          user: { id: user.id, name: user.name, pontos: user.pontos, posicao },
+          posicao,
+          rank: users,
+        });
+      }
+
+      return res.send({ rank: users });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error.messagem);
+    }
   }
 }
 
